00ea087db4491881dcb378e4126b3544f68565fc
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2023-11-20 13:55:04 +0700

    add(Culture.php): Add culture model

--- app/Models/Culture.php
+++ app/Models/Culture.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Culture extends Model
 {
     use HasFactory;
+
+    protected $table = "cultures";
+    protected $primarykey = "id";
+    protected $fillable = [
+        'province_id',
+        'category_id',
+        'name',
+        'img',
+        'video',
+        'desc'
+    ];
+
+    public function provinces(){
+        return $this->belongsTo(Province::class, 'province_id', 'id');
+    }
+    
+    public function categories(){
+        return $this->belongsTo(Categorie::class, 'category_id', 'id');
+    }
 }
