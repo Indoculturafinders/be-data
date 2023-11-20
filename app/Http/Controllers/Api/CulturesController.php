544ed163834d11a32893e337364a9a8e15b3bda2
Aulia Maharani <auliamaharani0909@gmail.com>
2023-11-20 14:54:55 +0700

    add(culturescontroller.php)add function

--- app/Http/Controllers/Api/CulturesController.php
+++ app/Http/Controllers/Api/CulturesController.php
@@ -66,6 +66,30 @@ return response()->json([
             'message' => 'Sukses memasukkan data'
         ], 201);
     }
+
+    public function searchByProvince(string $name)
+    {
+        $data = Culture::join('provinces', 'cultures.province_id', '=', 'provinces.id')
+            ->join('categories', 'cultures.category_id', '=', 'categories.id')
+            ->select('cultures.id', 'cultures.province_id', 'provinces.name as province_name', 'cultures.category_id', 'categories.name as category_name', 'cultures.name', 'cultures.img', 'cultures.video', 'cultures.desc')
+            ->where('provinces.name', 'like', "%$name%")
+            ->orderBy('cultures.id', 'asc')
+            ->get();
+
+        if ($data->isNotEmpty()) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Data Ditemukan',
+                'cultures' => $data,
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan',
+                'cultures' => $data,
+            ], 404);
+        }
+    }
     
     /**
      * Display the specified resource.
