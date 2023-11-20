b701a8c4b9d4a9f93d0d06145ec3ca3a1efef8f5
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2023-11-20 14:09:52 +0700

    add(CulturesController.php): Add function getById Culture

--- app/Http/Controllers/Api/CulturesController.php
+++ app/Http/Controllers/Api/CulturesController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Culture;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class CulturesController extends Controller
 {
@@ -28,7 +29,25 @@ class CulturesController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $data = Culture::join('provinces', 'cultures.province_id', '=', 'provinces.id')
+        ->join('categories', 'cultures.category_id', '=', 'categories.id')
+        ->select('cultures.id', 'cultures.province_id', 'provinces.name as province_name', 'cultures.category_id', 'categories.name as category_name', 'cultures.name', 'cultures.img', 'cultures.video', 'cultures.desc')
+        ->orderBy('cultures.id', 'asc')
+        ->find($id);
+
+        if ($data) {
+            return response()->json([
+                'status'=>true,
+                'message'=>'Data Ditemukan',
+                'cultures'=>$data,
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan',
+                'cultures' => $data
+            ], 404);
+        }
     }
 
     /**
