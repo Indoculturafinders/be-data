bc8edf0fe16e3bb094251be47ab35d7650d6c3d0
Muhammad Ilman Aqilaa <ilmanaqilaa2@gmail.com>
2023-11-26 20:41:48 +0700

    Add(RumahAdatCOntroller.php): add getAll, getByID, getByCatName, getByProvince

--- app/Http/Controllers/Api/RumahAdatController.php
+++ app/Http/Controllers/Api/RumahAdatController.php
@@ -24,7 +24,7 @@ class RumahAdatController extends Controller
         'status' => true,
         'message' => 'Data ditemukan',
         'result' => $data
-    ], 200);
+    ], 200); 
     }
 
     /**
