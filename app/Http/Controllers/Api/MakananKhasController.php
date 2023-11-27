30bf7e17d737494b26c45915e560102e2fbdde9d
Aulia Maharani <auliamaharani0909@gmail.com>
2023-11-27 11:32:33 +0700

    add(makanankhascontroller.php)add function searchbyname

--- app/Http/Controllers/Api/MakananKhasController.php
+++ app/Http/Controllers/Api/MakananKhasController.php
@@ -28,14 +28,6 @@ class MakananKhasController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(string $id)
@@ -62,19 +54,28 @@ class MakananKhasController extends Controller
         }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function searchByName(string $name)
     {
-        //
-    }
+        $data = Culture::join('provinces', 'cultures.province_id', '=', 'provinces.id')
+            ->join('categories', 'cultures.category_id', '=', 'categories.id')
+            ->select('cultures.id', 'cultures.province_id', 'provinces.name as province_name', 'cultures.category_id', 'categories.name as category_name', 'cultures.name', 'cultures.img', 'cultures.video', 'cultures.desc')
+            ->where('categories.name', '=', 'Makanan')
+            ->where('cultures.name', 'like', "%$name%")
+            ->orderBy('cultures.id', 'asc')
+            ->get();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        if ($data->isNotEmpty()) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Data Ditemukan',
+                'cultures' => $data,
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan',
+                'cultures' => $data,
+            ], 404);
+        }
     }
 }
