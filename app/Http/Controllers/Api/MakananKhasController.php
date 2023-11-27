2f921ad17e0b799b6bd83b5097354c23e86f6344
Aulia Maharani <auliamaharani0909@gmail.com>
2023-11-27 11:20:32 +0700

    add(MakananKhasController.php)add function index

--- app/Http/Controllers/Api/MakananKhasController.php
+++ app/Http/Controllers/Api/MakananKhasController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Culture;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class MakananKhasController extends Controller
 {
@@ -12,7 +13,18 @@ class MakananKhasController extends Controller
      */
     public function index()
     {
-        //
+        $data = Culture::join('provinces', 'cultures.province_id', '=', 'provinces.id')
+        ->join('categories', 'cultures.category_id', '=', 'categories.id')
+        ->select('cultures.id', 'cultures.province_id', 'provinces.name as province_name', 'cultures.category_id', 'categories.name as category_name', 'cultures.name', 'cultures.img', 'cultures.video', 'cultures.desc')
+        ->where('categories.name', '=', 'Makanan')
+        ->orderBy('cultures.id', 'asc')
+        ->get();
+
+    return response()->json([
+        'status' => true,
+        'message' => 'Data ditemukan',
+        'result' => $data
+    ], 200);
     }
 
     /**
