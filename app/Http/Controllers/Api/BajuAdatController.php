981ff542f2a2facf76fd030504552664e82d7fb0
Aulia Maharani <auliamaharani0909@gmail.com>
2023-12-02 17:11:33 +0700

    add(BajuAdatContoller.php)add new function

---
+++ app/Http/Controllers/Api/BajuAdatController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Culture;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class BajuAdatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+   
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+      
+    }
+
+
+    public function searchByName(string $name)
+    {
+        
+    }
+
+    public function searchByProvince(string $name)
+    {
+       
+    }
+}
