46615700d859cde865a4d716b1135fb8b6e012f3
Aulia Maharani <auliamaharani0909@gmail.com>
2023-12-02 17:02:45 +0700

    add(MakananKhasController.php)add function searchbyprovince

--- app/Http/Controllers/Api/MakananKhasController.php
+++ app/Http/Controllers/Api/MakananKhasController.php
@@ -78,4 +78,29 @@ class MakananKhasController extends Controller
             ], 404);
         }
     }
+
+    public function searchByProvince(string $name)
+    {
+        $data = Culture::join('provinces', 'cultures.province_id', '=', 'provinces.id')
+            ->join('categories', 'cultures.category_id', '=', 'categories.id')
+            ->select('cultures.id', 'cultures.province_id', 'provinces.name as province_name', 'cultures.category_id', 'categories.name as category_name', 'cultures.name', 'cultures.img', 'cultures.video', 'cultures.desc')
+            ->where('categories.name', '=', 'Makanan')
+            ->where('provinces.name', 'like', "%$name%")
+            ->orderBy('cultures.id', 'asc')
+            ->get();
+
+        if ($data->isNotEmpty()) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Data Ditemukan',
+                'cultures' => $data,
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan',
+                'cultures' => $data,
+            ], 404);
+        }
+    }
 }
